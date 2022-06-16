687fc3677ac01024bfb663a86fbad4267dd01769
Martin Burolla <mburolla@xpanxion.com>
2022-06-16 17:17:33 -0400

    update ex6

--- src/Student0/Ex6/SentenceBuilder.js
+++ src/Student0/Ex6/SentenceBuilder.js
@@ -6,24 +6,40 @@ import './SentenceBuilder.scss'
 export const SentenceBuilder = () => {
     let inputRef = useRef(null)
     let [sentence, setSentence] = useState("")
+    let [counter, setCounter] = useState(0)
+    let [disabled, setDisabled] = useState(true)
 
     const onHandleWord = () => {
         let completeSentence = sentence + " " + inputRef.current.value
         setSentence(completeSentence)
         inputRef.current.value = ""
+        setCounter(++counter)
+        setDisabled(true)
     }
 
     const onHandleReset = () => {
         setSentence("")
+        setCounter(0)
+        setDisabled(true)
+        inputRef.current.value = ""
+    }
+
+    const onWordChange = () => {
+        if (inputRef.current.value.length > 0) {
+            setDisabled(false)
+        } else {
+            setDisabled(true)
+        }
     }
 
     return (
         <div>
             <h2>Sentence Builder</h2>
             <div>Sentence: {sentence}</div>
-            <input ref={inputRef}></input>
-            <button onClick={() => onHandleWord()}>Add Word</button>
-            <button onClick={() => onHandleReset()}>Reset</button>
+            <div>Word Counter: {counter}</div>
+            <input ref={inputRef} onChange={ () => onWordChange()}></input>
+            <button onClick={() => onHandleWord()} disabled={disabled}>Add Word</button>
+            <button onClick={() => onHandleReset()} disabled={disabled}>Reset</button>
         </div>
     )
 }
