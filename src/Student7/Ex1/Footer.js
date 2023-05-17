7a15729312f6c25fe0554d21e3204670814bf728
Karina Velasquez <kvelasquez@xpanxion.com>
2023-05-17 12:47:34 -0400

    Commit Update with ex1

---
+++ src/Student7/Ex1/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './footer.css'
+
+export const Footer = () => {
+    return (
+      <div class="Footer">
+          Copyright 2022
+      </div>
+    )
+  }
+  
---
+++ src/Student7/Ex1/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './nav.css'
+
+export const Nav = () => {
+  return (
+    <div className="Nav">
+        Home | Login | Support | FAQ
+    </div>
+  )
+}
