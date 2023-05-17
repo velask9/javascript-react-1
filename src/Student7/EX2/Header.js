4e72ecc272814afe95d03c01b10e9ade47102387
Karina Velasquez <kvelasquez@xpanxion.com>
2023-05-17 15:23:42 -0400

    Update with EX2

---
+++ src/Student7/EX2/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Header.css'
+
+
+export const Header = ()=>{
+    return (
+			<div>
+				<div className='header'>
+					<h1>FootBook</h1>
+				</div>
+			</div>
+		);
+}
---
+++ src/Student7/EX2/Nav.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Nav.css'
+
+export const Nav = () => { 
+    return(
+        <div className="Nav"> 
+        Home | FAQ | Support | Account
+        </div>
+    )
+ }
